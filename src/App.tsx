8436fb4a0a8566f3758159a9f3aa10446c69b177
Akshay <akshay.goyal1008@gmail.com>
2024-03-14 18:44:25 +0530

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./component/Dashboard";
+import List from "./component/AlbumList";
+import { FavoritesProvider } from "./store/FavoritesContext";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <FavoritesProvider>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
+      </FavoritesProvider>
+    </Router>
   );
-}
+};
 
 export default App;
